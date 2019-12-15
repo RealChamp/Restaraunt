6007bd640b1638276dd2ae39b45b1ee0f42a8779
Matvey <matvey.w2015@mail.ru>
2019-12-15 17:38:33 +0400

    find-us section done

--- app/js/main.js
+++ app/js/main.js
@@ -2,8 +2,11 @@ $(function () {
 
     $('.online-menu').slick({
         arrows: false,
+        dots: true,
         infinite: true,
         fade: true,
+        autoplay: true,
+        autoplaySpeed: 3500,
     });
 
     new WOW().init();
